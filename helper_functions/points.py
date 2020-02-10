6b233de2244c6e1df363aca2dda0252ce118c5a8
daniel <dannymevs@hotmail.com>
2020-02-09 19:00:17 -0500

    my second commit

--- helper_functions/points.py
+++ helper_functions/points.py
@@ -11,50 +11,59 @@ def binarify(Alist):
         Alist[i] = random.randint(0,1)
     return Alist
 
-def binary_negation(Alist):
-    for i in range(len(Alist)):
-        if Alist[i] == 0:
-            Alist[i] = 1
-        elif Alist[i] == 1:
-            Alist[i] = 0
-    return Alist
+def binary_convert(d):
+        if d == 0:
+            d = 1
+            return d
+        if d == 1:
+            d = 0
+            return d
+
+
 
 def purify(Alist):
     for i in range(len(Alist)):
         Alist[i] = 0
     return Alist
 
+#algorithm that keeps count of all possible ways to arrange binary values in a list of size 4 (2^4) 
+def brute_cartesian():
+    count = 0
+    arr1 = [0,0,0,0]
+    count+=1
 
-def brute_cartesian(aList):
-    i = len(aList)-1
-    j = 0
-    count = 1 # for initial purified list
-    while(i >= 0):
-        if(count == 1):
-            aList[i] = 1
-            aList = purify(aList[:i])
-            
-            count += 1
-            
-        else:
-            i -= 1
-            aList[i] = 1
-            count += 1
-            j = i+1
-            aList[j] = 0
+    arr2 = [1,1,1,1]
+    count+=1
+
+    print(arr1,'      ', arr2)
+
+    bi1 = 1 #binary digit for array 1
+    bi2 = 0 #binary digit for array 2
+
+    i =0
+        
+
+    n =len(arr1)-1
+    m = 1
+
+    while(m <= n+1):
+        
+        while(i <= n-m):
+            arr1[i]=bi1
             count+=1
-            
-            while(j <= len(aList)-1):
-                 if(j == len(aList)-1):
-                    while(j > i):
-                        aList[j] = 1
-                        #count+=1
-                j = j+i
-                    count+=1
-                else:
-                    aList[j] = 1
-                    count+=1
-                    aList[j] = 0
+            arr2[i]=bi2
+            count+=1
+            print(arr1,'      ', arr2)
+            i+=1
+        m+=1
+        if(n-m >= 0):
+            i= 0
+        else:
+            i = -1
+            bi1 = binary_convert(bi1)
+            bi2 = binary_convert(bi2)
+        bi1 = binary_convert(bi1)
+        bi2 = binary_convert(bi2)
                 
                
                         
@@ -65,15 +74,7 @@ def brute_cartesian(aList):
 
 
 
-x = populate(4)
-print(x)
 
-count = brute_cartesian(x)
-print('expected answer: 16')
-print('actual answer: ', count)
-            
-'''x = binary_negation(x)
-print(x)'''
         
     
     
